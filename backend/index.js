95cd0afa7a26931a062fa685dc6a2dce74366a70
kimjulia1117 <kimjulia1117@gmail.com>
2019-11-09 13:55:24 -0600

    Setting up port

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,44 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
+//
+//Experimenting using cheerio
+// const rp = require('request-promise');
+// const cheerio = require('cheerio');
 
-const options = {
-    uri: `https://www.nytimes.com/2019/11/04/upshot/trump-biden-warren-polls.html`,
-    transform: function (body) {
-        return cheerio.load(body);
-    }
-};
+// const options = {
+//     uri: `https://www.nytimes.com/2019/11/04/upshot/trump-biden-warren-polls.html`,
+//     transform: function (body) {
+//         return cheerio.load(body);
+//     }
+// };
 
-rp(options)
-    .then(($) => {
-        console.log($);
+// rp(options)
+//     .then(($) => {
+//         console.log($);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+const db = require('./queries')
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
     })
-    .catch((err) => {
-        console.log(err);
-    });
+)
+
+app.get('/', (request, response) => {
+    response.json({info: 'Node.js API' })
+})
+
+app.get('/health', (request, response) => {
+    response.json({message: 'GET HEALTH STATUS : OK !'})
+})
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
