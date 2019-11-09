366d91ea4b341f46243d9145b57b98606cd240eb
kimjulia1117 <kimjulia1117@gmail.com>
2019-11-09 14:02:43 -0600

    Connect to port 3000, also added express

--- backend/index.js
+++ backend/index.js
@@ -22,21 +22,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
-const db = require('./queries')
 
 app.use(bodyParser.json())
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 )
 
 app.get('/', (request, response) => {
     response.json({info: 'Node.js API' })
-})
-
-app.get('/health', (request, response) => {
-    response.json({message: 'GET HEALTH STATUS : OK !'})
 })
 
 app.listen(port, () => {
