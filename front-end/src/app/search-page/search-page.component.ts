636244767d1434c4a277b17864fc0f1bdb934e63
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2019-11-10 01:01:44 -0600

    get_json works with the test file

--- front-end/src/app/search-page/search-page.component.ts
+++ front-end/src/app/search-page/search-page.component.ts
@@ -16,32 +16,25 @@ export class SearchPageComponent implements OnInit {
   ngOnInit() {
   }
 
-  /*Function getData() will be called when the user hit the Bias Check button, 
-   *it will call the getWebsiteURL function which will return the JSON payload 
-   */
-  getData() {
-    var analysis_results = this.getWebsiteURL();
-    console.log(analysis_results);
-  }
-
   /*Function getWebsiteURL() will be called when the getData function calls it
    *it gets the information in the textbox which will be a url link, this link
    *will be passed to the getJSON function and will get the JSON payload from the api call
    */
   getWebsiteURL() {
     var website = (<HTMLInputElement>document.getElementById("url")).value;
-    return this.getJSON(website);
+    this.get_json(website);
   }
 
-  /*Function getJSON takes in a one parameter website which is the url that the api will call.
+  /*Function get_json takes in a one parameter website which is the url that the api will call.
    *This function returns the JSON payload that is the result of the call.
-   */
-  getJSON(website) {
-    return this.http.jsonp("https://raw.githubusercontent.com/nich227/ncm-utd/master/src/assets/events.json", 'callback');
+   */ 
+  get_json(website) {
+    $.getJSON("https://raw.githubusercontent.com/nich227/ncm-utd/master/src/assets/events.json", function (json) {
     // return this.http.jsonp("localhost:3000/content?url=website", 'callback');
+      console.log(JSON.stringify(json));
+      //bias-score
+      //top-bias-phrases (is json form)
+    });
   }
-
-
   
-
 }
