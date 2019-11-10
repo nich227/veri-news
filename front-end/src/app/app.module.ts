02d713fcd38a4d3558ac20d0342bb6c243813c63
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2019-11-09 21:15:34 -0600

    Added the result-page component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ResultsPageComponent } from './results-page/results-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchPageComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ResultsPageComponent
   ],
   imports: [
     BrowserModule,
