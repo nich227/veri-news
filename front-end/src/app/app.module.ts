538784ed97cfc98447fa552ce7c651951e84ee9e
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2019-11-09 19:02:09 -0600

    Edited the header components to a good for now state

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchPageComponent } from './search-page/search-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
