549680b567cb8b22aeb3b504de52ba082d853de3
Kevin Chen <ningchen227@gmail.com>
2019-11-10 10:10:14 -0600

    Move pushing out

--- front-end/src/app/results-page/results-page.component.ts
+++ front-end/src/app/results-page/results-page.component.ts
@@ -1,38 +1,39 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { CloudData, CloudOptions } from 'angular-tag-cloud-module';
-import { StateService } from '../state.service';
+import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
+import { CloudData, CloudOptions } from "angular-tag-cloud-module";
+import { StateService } from "../state.service";
 
 @Component({
-  selector: 'app-results-page',
-  templateUrl: './results-page.component.html',
-  styleUrls: ['./results-page.component.sass']
+  selector: "app-results-page",
+  templateUrl: "./results-page.component.html",
+  styleUrls: ["./results-page.component.sass"]
 })
 export class ResultsPageComponent implements OnInit {
   bias: string;
   options: CloudOptions = {
-    // if width is between 0 and 1 it will be set to the size of the upper element multiplied by the value 
+    // if width is between 0 and 1 it will be set to the size of the upper element multiplied by the value
     width: 1,
     height: 300,
-    overflow: false,
+    overflow: false
   };
- 
-  data: CloudData[] = [
-    
-  ];
 
-  constructor(private state: StateService) { }
-  
+  data: CloudData[] = [];
+
+  constructor(private state: StateService) {}
+
   ngOnInit() {
-    this.state.json.subscribe(bias => this.bias = bias);
+    this.state.json.subscribe(bias => (this.bias = bias));
     //console.log(this.bias);
     let jsonObj = JSON.parse(this.bias);
     let final_score = JSON.parse(this.bias).bias_score * 10;
     $(document).ready(function() {
-      $('#lin-gauge').attr('value', final_score);
-      console.log(jsonObj.top_bias_phrases);
-      JSON.parse(jsonObj.top_bias_phrases).forEach(obj => {
-        this.data.push({text: obj.text, weight: obj.sentiment, color: '#ffffff'});
+      $("#lin-gauge").attr("value", final_score);
+    });
+    JSON.parse(jsonObj.top_bias_phrases).forEach(obj => {
+      this.data.push({
+        text: obj.text,
+        weight: obj.sentiment,
+        color: "#ffffff"
       });
     });
   }
