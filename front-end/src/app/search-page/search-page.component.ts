3a2eb75b752e22e2a39d695dfc7a3a1ed8b7f8a5
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2019-11-10 00:02:13 -0600

    Added getData() and getJSON() functions that will get the JSON output from the api

--- front-end/src/app/search-page/search-page.component.ts
+++ front-end/src/app/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-search-page',
@@ -7,15 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+  getData() {
+    var analysis_results = this.getWebsiteURL();
+    console.log(analysis_results);
+  }
+
   getWebsiteURL() {
     var website = (<HTMLInputElement>document.getElementById("url")).value;
-    console.log(website);
+    // return this.http.jsonp("localhost:3000/content?url=website", 'callback');
+    return this.getJSON(website);
+    //console.log(this.http.jsonp("https://raw.githubusercontent.com/nich227/ncm-utd/master/src/assets/events.json", 'callback'));
   }
+
+  getJSON(website) {
+    return this.http.jsonp("https://raw.githubusercontent.com/nich227/ncm-utd/master/src/assets/events.json", 'callback');
+    // return this.http.jsonp("localhost:3000/content?url=website", 'callback');
+  }
+
 
   
 
