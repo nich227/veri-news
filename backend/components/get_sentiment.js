fddfa51d4f0ee5f8aa81cf8e4a73be3bb76fe3ce
78080D <780dev@gmail.com>
2019-11-09 19:42:17 -0600

    Testing with different outputs

--- backend/components/get_sentiment.js
+++ backend/components/get_sentiment.js
@@ -5,9 +5,7 @@ var NaturalLanguageUnderstandingV1 = require("ibm-watson/natural-language-unders
 require("dotenv").config({ silent: true }); //  optional
 
 var nlu = new NaturalLanguageUnderstandingV1({
-  //authenticator: new IamAuthenticator({ apikey: "eEvNCsoXt_QRo4WbUk-mnAX1xAVBb8-WoKFJeecrKQ3_" }),
   version: "2018-04-05"
-  //url: "https://gateway.watsonplatform.net/natural-language-understanding/api/"
 });
 
 var filename = "../res/test.html";
@@ -36,6 +34,57 @@ fs.readFile(filename, "utf-8", function(file_error, file_data) {
         return;
       }
       let output = JSON.stringify(res);
+      console.log("OUT: " + output);
+      let jsonObj = JSON.parse(output);
+      let sum = 0, sumRelevance = 0;
+      let numWords = 0;
+      let relevanceScores = [], relevanceSquareDiffs = [];
+      let avgEmotionScore = 0;
+      jsonObj.result.keywords.forEach (word => {
+        let emotionScore = (word.emotion.sadness + word.emotion.joy + word.emotion.fear + word.emotion.disgust + word.emotion.anger)/5;
+        console.log("Sent: " + word.sentiment.score + " // Emo. Avg: " + emotionScore + " // Rel: " + word.relevance + " // Text: " + word.text);
+        sum += Math.abs(word.sentiment.score);
+        avgEmotionScore += emotionScore;
+        relevanceScores.push(word.relevance);
+        numWords++;
+      })
+      console.log("Words: " + numWords)
+      console.log("Average sentiment: " + (sum/numWords));
+      console.log("Average Emotional Score: " + (avgEmotionScore/numWords));
+      //console.log("Words: " + numWords);
+      //console.log("Relevance scores: " + relevanceScores);
+      /*
+      let rSum = 0;
+      relevanceScores.forEach(val => {
+        rSum += val;
+      })
+      let rAvg = rSum/relevanceScores.length;
+      //console.log("Relevance Avg: " + rAvg);
+      let rStdSummation = 0;
+      relevanceScores.forEach(val => {
+        relevanceSquareDiffs.push(Math.pow((val - rAvg), 2));
+      })
+      let sqDiffAvg = 0;
+      relevanceSquareDiffs.forEach(val => {
+        sqDiffAvg += val;
+      })
+      sqDiffAvg = sqDiffAvg/relevanceSquareDiffs.length;
+
+      let rStdDev = Math.sqrt(sqDiffAvg);
+      console.log("Relevance avg: " + rAvg + ", standard dev: " + rStdDev);
+      let excludeLimit = rAvg - rStdDev;
+
+      sum = 0;
+      numWords = 0;
+      jsonObj.result.keywords.forEach (word => {
+        if(word.relevance > rAvg) {
+          sum += word.sentiment.score;
+          numWords++;
+        }
+      })
+      console.log("Improved sentiment avg: " + (sum/numWords));
+      console.log("Words: " + numWords);
+      */
     });
   }
 });
