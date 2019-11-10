4941b71f26a93977af58752d4db9b84678b92faa
kimjulia1117 <kimjulia1117@gmail.com>
2019-11-09 23:00:13 -0600

    Change filename path

--- backend/components/get_sentiment.js
+++ backend/components/get_sentiment.js
@@ -12,7 +12,7 @@ module.exports = function() {
   var bias_score = 0;
   var top_bias_phrases = [];
 
-  var filename = "../backend/res/test.html";
+  var filename = "../backend/outputFile.html";
   fs.readFile(filename, "utf-8", function(file_error, file_data) {
     if (file_error) {
       console.log(file_error);
