b1e18cce7a747c10312553e114652baf55d88140
Patrick Soisson <32037100+PSoysauce@users.noreply.github.com>
2019-11-10 00:06:36 -0600

    Added comments to explain the search-page.component.ts functions

--- front-end/src/app/search-page/search-page.component.ts
+++ front-end/src/app/search-page/search-page.component.ts
@@ -16,18 +16,26 @@ export class SearchPageComponent implements OnInit {
   ngOnInit() {
   }
 
+  /*Function getData() will be called when the user hit the Bias Check button, 
+   *it will call the getWebsiteURL function which will return the JSON payload 
+   */
   getData() {
     var analysis_results = this.getWebsiteURL();
     console.log(analysis_results);
   }
 
+  /*Function getWebsiteURL() will be called when the getData function calls it
+   *it gets the information in the textbox which will be a url link, this link
+   *will be passed to the getJSON function and will get the JSON payload from the api call
+   */
   getWebsiteURL() {
     var website = (<HTMLInputElement>document.getElementById("url")).value;
-    // return this.http.jsonp("localhost:3000/content?url=website", 'callback');
     return this.getJSON(website);
-    //console.log(this.http.jsonp("https://raw.githubusercontent.com/nich227/ncm-utd/master/src/assets/events.json", 'callback'));
   }
 
+  /*Function getJSON takes in a one parameter website which is the url that the api will call.
+   *This function returns the JSON payload that is the result of the call.
+   */
   getJSON(website) {
     return this.http.jsonp("https://raw.githubusercontent.com/nich227/ncm-utd/master/src/assets/events.json", 'callback');
     // return this.http.jsonp("localhost:3000/content?url=website", 'callback');
